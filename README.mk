aa508f5337f780eefb960fc4d2186035bdd9a840
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-08-19 11:52:47 +0200

    update readme

--- README.mk
+++ README.mk
@@ -1,17 +1,19 @@
-# Jawaker Cards Counter
+#Jawaker Cards Counter
 
 Jawaker Cards Counter is a Chrome Extension that counts played cards inside [Jawaker](https://jawaker.com/) game.
 
-### How To Use:
-  1. Add the extension to chrome by [following this guide](https://developer.chrome.com/extensions/getstarted#unpacked)
-  2. you should have the extension icon now added and showing next url textbox
-  3. log into Jawaker and join a game
+1. Add the extension to chrome by [following this guide](https://developer.chrome.com/extensions/getstarted#unpacked)
+2. you should have the extension icon now added and showing next url textbox
+3. log into Jawaker and join a game
 4. make sure that Jawaker is the currently active tab 
-5. Open the extension and click on Inject (this will inject cards counting script into Jawaker page)  
-  6. every time a card is played you can click on the Extension button and  the popup window  should show which cards hasn't been played yet.
+5. Open the extension and click on Inject (this will inject cards counting script into Jawaker page)
+6. every time a card is played you can click on the Extension button and  the popup window  should show which cards hasn't been played yet.
+
 
 
 **Note:** after you finish a game either click on the delete button inside the extension popup window, or referesh the browser.
+
+
 
 
 
